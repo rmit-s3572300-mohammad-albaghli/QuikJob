a9d91be46675da3da0b5d1b22822028d58e76a1f
EC2 Default User <ec2-user@ip-172-31-80-8.ec2.internal>
2019-08-14 09:30:43 +0000

    Added static pages with bootstrap and links for contact and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  #routes for static pages
+  
+  
+
+  get '/home', to: 'static_pages#Home'
+  get '/about', to: 'static_pages#About'  
+  get '/contact', to: 'static_pages#Contact'
+  
+  #get 'static_pages/Home'
+  #get 'static_pages/About'
+  #get 'static_pages/Contact'
+  root 'static_pages#Home'
+
   # Routes for error pages
   get '/404', to: "errors#not_found"
   get '/422', to: "errors#unacceptable"
