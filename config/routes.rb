38d75ba151bc5679956db5a877cb6cd46738627d
Kevin Huang <s3556040@student.rmit.edu.au>
2019-08-14 10:41:45 +0000

    Added signup page for jobseekers

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,19 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#Home'
   get '/about', to: 'static_pages#About'  
   get '/contact', to: 'static_pages#Contact'
+  
+  #Routes for employer model
   get  '/employer_signup',  to: 'employers#new'
   post '/employer_signup',  to: 'employers#create'
+  
+  #Routes for jobseeker model
+  get '/jobseeker_signup', to: 'jobseekers#new'
+  post '/jobseeker_signup', to: 'jobseekers#create'
   
   # Routes for error pages
   get '/404', to: "errors#not_found"
   get '/422', to: "errors#unacceptable"
   get '/500', to: "errors#server_errors"
   
-  resources :employers
+  resources :employers, :jobseekers
 end
