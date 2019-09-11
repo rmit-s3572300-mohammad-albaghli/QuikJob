6bca80d03bba431b14a54abd35d8f397faf49ac1
Kevin Huang <s3556040@student.rmit.edu.au>
2019-09-11 05:04:23 +0000

    Added description field to jobseekers.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190828084652) do
+ActiveRecord::Schema.define(version: 20190911045402) do
 
   create_table "employers", force: :cascade do |t|
     t.string "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20190828084652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "description"
   end
 
   create_table "jobseekers_skills", id: false, force: :cascade do |t|
