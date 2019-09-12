601c39352c0e92a450936eab3a82c38206a6601e
Brandon-lee Love <s3656231@student.rmit.edu.au>
2019-09-12 03:13:04 +0000

    added adding skills via file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190911062719) do
+ActiveRecord::Schema.define(version: 20190912025543) do
 
   create_table "employers", force: :cascade do |t|
     t.string "name"
